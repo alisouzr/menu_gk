6aba98c2c775a0ffad91d689cf6f3525af2094c6
Aliny Souza Ramos <souzaramosaliny@gmail.com>
2024-12-04 09:53:06 -0400

    feat: added search box

--- script.js
+++ script.js
@@ -212,3 +212,17 @@ if(isOpen){
     spanItem.classList.remove("bg-green-600");
 }
 
+function search_product() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('flex gap-2');
+      
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
