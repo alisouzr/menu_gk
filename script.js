ec5aed623277ebc217a7126b84ec2b07438cd44d
Aliny Souza Ramos <souzaramosaliny@gmail.com>
2024-12-04 10:03:00 -0400

    feat: bug finished

--- script.js
+++ script.js
@@ -215,7 +215,7 @@ if(isOpen){
 function search_product() {
     let input = document.getElementById('searchbar').value
     input=input.toLowerCase();
-    let x = document.getElementsByClassName('flex gap-2');
+    let x = document.getElementsByClassName('product');
       
     for (i = 0; i < x.length; i++) { 
         if (!x[i].innerHTML.toLowerCase().includes(input)) {
